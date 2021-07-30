61109ba1659494658da17798b230cc4060dceb4f
sergiojanko <janko.sergio1998@gmail.com>
2021-07-30 15:40:15 +0200

    add-and-remove-tasks

--- js/script.js
+++ js/script.js
@@ -2,8 +2,15 @@ const app = new Vue ({
     el: "#app",
     data: {
         tasks: ["Lavare l'auto","Fare benzina","Fare la spesa"],
+        newTask: "",
     },
     methods: {
-
+        removeTask(i){
+            this.tasks.splice(i,1);
+        },
+        addTask() {
+            this.tasks.push(this.newTask);
+            this.newTask = "";
+        }
     }
 });
